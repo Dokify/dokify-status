3182033bba868f7834c14562c3f535118b0849d6
asanchez <asanchez@asanchez-UX31E.(none)>
2012-09-13 15:49:42 +0200

    Ajustes en imagen de progreso y CSS

--- js/main.js
+++
@@ -1,4 +0,0 @@
-function callprogress(vValor){
- document.getElementById("getprogress").innerHTML = vValor;
- document.getElementById("getProgressBarFill").innerHTML = '<div class="ProgressBarFill" style="width: '+vValor+'%;"></div>';
-}
