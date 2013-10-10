845a98fe6c32d51020b0e6e8c955533e4ffa6972
Alberto Sánchez <asanchez@dokify.net>
2013-10-10 13:04:00 +0200

    eliminar mustache

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 	require 'class/AWSStatus.class.php';
-	require 'vendor/mustache/mustache/src/Mustache/Autoloader.php';
-	Mustache_Autoloader::register();
+	require_once 'vendor/twig/twig/lib/Twig/Autoloader.php';
+	Twig_Autoloader::register();
+	//require 'vendor/mustache/mustache/src/Mustache/Autoloader.php';
+	//Mustache_Autoloader::register();
 
 	function dump($var){
 		print '<pre>'. print_r($var, true) . '</pre>';
